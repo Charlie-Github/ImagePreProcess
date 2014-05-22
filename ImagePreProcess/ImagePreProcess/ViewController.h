b354dac210ccbad365d82ae991d39f7809e7050f
Charlie Gao <gcte2010@gmail.com>
2014-05-22 15:44:24 -0500

    commit new project
    commit new project for testing openCV

---
+++ ImagePreProcess/ImagePreProcess/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImagePreProcess
+//
+//  Created by CharlieGao on 5/22/14.
+//  Copyright (c) 2014 Edible Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
